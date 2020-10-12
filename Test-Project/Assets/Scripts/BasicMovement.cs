32a189edeafcd7c875e1bdc4c3065e62ac3e488d
brsp205 <71081365+brsp205@users.noreply.github.com>
2020-10-12 23:11:32 +0200

    +Rotation, Cursor

--- Test-Project/Assets/Scripts/BasicMovement.cs
+++ Test-Project/Assets/Scripts/BasicMovement.cs
@@ -5,25 +5,51 @@ using System;
 
 public class BasicMovement : MonoBehaviour
 {
+
+    private Camera cam;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        cam = Camera.main;
     }
 
     public Animator animator;
-
+    
     // Update is called once per frame
     void Update()
     {
-        animator.SetFloat("Horizontal", Math.Abs(Input.GetAxis("Vertical"))+Math.Abs(Input.GetAxis("Horizontal")));
-        //animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
-        //animator.SetFloat("Vertical", Input.GetAxis("Vertical"));
-        float speed = 4f;
-        //Vector3 speeds      = new Vector3(0.3f,0.3f,0.3f);
+
+        if (Math.Abs(Input.GetAxis("Vertical"))+Math.Abs(Input.GetAxis("Horizontal")) == 0)
+        {
+            animator.SetInteger("Stopped",1);
+        }
+        else
+        {
+            animator.SetInteger("Stopped",0);
+        }
+
+        float speed = 8f;
+
         Vector3 horizontal  = new Vector3(Input.GetAxis("Horizontal")   , 0.0f                      , 0.0f);
         Vector3 vertical    = new Vector3(0.0f                          , Input.GetAxis("Vertical") , 0.0f);
+
         transform.position  = transform.position + horizontal * Time.deltaTime * speed;
         transform.position  = transform.position + vertical * Time.deltaTime * speed;
+
+
+        Vector3 mouse = Input.mousePosition;
+        //Debug.Log("Mouse Input " + mouse.ToString("F3"));
+        Vector3 point = cam.ScreenToWorldPoint(mouse);
+        //Debug.Log("Point " + point.ToString("F3"));
+        double interx = point.x - transform.position.x ;
+        double intery = point.y - transform.position.y ;
+        Vector3 rota = new Vector3(0.0f , 0.0f , Convert.ToSingle(Math.Atan2(intery,interx)/Math.PI*180) );
+        float rotb = transform.eulerAngles.z;
+        //Debug.Log("rotaZ " + rota.z.ToString("F3"));
+        //Debug.Log("rota read " + transform.eulerAngles.ToString("F3"));
+        transform.Rotate(0.0f , 0.0f , rota.z-rotb,Space.Self);
+        //transform.Rotate(0.0f , 0.0f , 0.01f ,Space.Self);
+
     }
 }
---
+++ Test-Project/Assets/Scripts/Cursor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Cursor : MonoBehaviour
+{
+
+
+    private Camera cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        Vector3 mouse = Input.mousePosition;
+        Vector3 point = cam.ScreenToWorldPoint(mouse);
+        transform.position  = new Vector3(point.x,point.y,0.0f);
+
+
+    }
+}
