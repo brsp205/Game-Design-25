b738c15f78a0e47ef0b9c1716497b869ec09cc73
brsp205 <71081365+brsp205@users.noreply.github.com>
2020-10-13 17:06:28 +0200

    +Camera
    Character stays centered on screen

--- Test-Project/Assets/Scripts/BasicMovement.cs
+++ Test-Project/Assets/Scripts/BasicMovement.cs
@@ -7,7 +7,7 @@ public class BasicMovement : MonoBehaviour
 {
 
     private Camera cam;
-
+    public static Vector3 externpos;    
     // Start is called before the first frame update
     void Start()
     {
@@ -19,7 +19,7 @@ public class BasicMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        //If arrows or wasd are pressed Stopped variable set to 0 to start animation
         if (Math.Abs(Input.GetAxis("Vertical"))+Math.Abs(Input.GetAxis("Horizontal")) == 0)
         {
             animator.SetInteger("Stopped",1);
@@ -29,27 +29,29 @@ public class BasicMovement : MonoBehaviour
             animator.SetInteger("Stopped",0);
         }
 
-        float speed = 8f;
+        float speed = 8f; // run speed
 
-        Vector3 horizontal  = new Vector3(Input.GetAxis("Horizontal")   , 0.0f                      , 0.0f);
-        Vector3 vertical    = new Vector3(0.0f                          , Input.GetAxis("Vertical") , 0.0f);
-
+        Vector3 horizontal  = new Vector3(Input.GetAxis("Horizontal")   , Input.GetAxis("Vertical") , 0.0f);
         transform.position  = transform.position + horizontal * Time.deltaTime * speed;
-        transform.position  = transform.position + vertical * Time.deltaTime * speed;
+        //Public variable that is visible to other classes. Contains Vector3 format: (x,y,z) of the character in game units
+        externpos = transform.position;
 
-
+        //Get mouseinput in pixels
         Vector3 mouse = Input.mousePosition;
-        //Debug.Log("Mouse Input " + mouse.ToString("F3"));
+            //Debug.Log("Mouse Input " + mouse.ToString("F3")); //Prints mouse to console 
+        //Convert mouseinput to game units
         Vector3 point = cam.ScreenToWorldPoint(mouse);
-        //Debug.Log("Point " + point.ToString("F3"));
-        double interx = point.x - transform.position.x ;
+            //Debug.Log("Point " + point.ToString("F3"));   //Prints point to console
+
+        double interx = point.x - transform.position.x ; //difference between mouse and character location
         double intery = point.y - transform.position.y ;
-        Vector3 rota = new Vector3(0.0f , 0.0f , Convert.ToSingle(Math.Atan2(intery,interx)/Math.PI*180) );
+        Vector3 rota = new Vector3(0.0f , 0.0f , Convert.ToSingle(Math.Atan2(intery,interx)/Math.PI*180) );//atan2
         float rotb = transform.eulerAngles.z;
-        //Debug.Log("rotaZ " + rota.z.ToString("F3"));
-        //Debug.Log("rota read " + transform.eulerAngles.ToString("F3"));
+            //Debug.Log("rotaZ " + rota.z.ToString("F3"));
+            //Debug.Log("rota read " + transform.eulerAngles.ToString("F3"));
         transform.Rotate(0.0f , 0.0f , rota.z-rotb,Space.Self);
-        //transform.Rotate(0.0f , 0.0f , 0.01f ,Space.Self);
+
 
     }
+    
 }
---
+++ Test-Project/Assets/Scripts/Camera_f.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+public class Camera_f : MonoBehaviour
+{
+
+
+    private Camera cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float magX = 0.5f; 
+        float magY = 0.3f;
+        //double length = Math.Sqrt(Math.Pow(transform.position.x-BasicMovement.externpos.x,2)+Math.Pow(transform.position.y-BasicMovement.externpos.y,2));
+        
+        Vector3 movecam  = new Vector3((BasicMovement.externpos.x-transform.position.x)/magX , (BasicMovement.externpos.y-transform.position.y)/magY , 0.0f);
+        transform.position  = transform.position + movecam*Time.deltaTime;
+        //Debug.Log("Char Pos " + movecam.ToString("F3"));
+        
+
+
+    }
+}
+
